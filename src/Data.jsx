4b992f0b4f95b3b1d41eab15b7f5ae6be5cc0d0b
ogabido somto <112892790+ogabz-byte@users.noreply.github.com>
2022-10-08 20:04:02 +0100

    Update Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -14,12 +14,47 @@ const data = {
       image:
         "https://images.unsplash.com/photo-1544896478-d5b709d413c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmludGFnZSUyMGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
     },
-    {
+     {
       id: 3,
-      name: "Shoes",
-      price: 100,
+      name: "Playstation 5",
+      price: 6400,
       image:
-        "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+        "https://images.unsplash.com/photo-1607853202273-797f1c22a38e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxheXN0YXRpb24lMjA1fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+     {
+      id: 4,
+      name: "Camera",
+      price: 4400,
+      image:
+        "https://images.unsplash.com/photo-1552168324-d612d77725e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FtZXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+     {
+      id: 5,
+      name: "Rolex",
+      price: 2400,
+      image:
+        "https://images.unsplash.com/photo-1587836374828-4dbafa94cf0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cm9sZXh8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+     {
+      id: 6,
+      name: "Iphone 12",
+      price: 3400,
+      image:
+        "https://images.unsplash.com/photo-1523206489230-c012c64b2b48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aXBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+     {
+      id: 7,
+      name: "Air pod",
+      price: 1400,
+      image:
+        "https://images.unsplash.com/photo-1610438235354-a6ae5528385c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWlycG9kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 8,
+      name: "Router",
+      price: 1100,
+      image:
+        "https://images.unsplash.com/photo-1606420187127-dae7c868fa7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cm91dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     },
   ],
 };
